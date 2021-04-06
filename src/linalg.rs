e5abac739ce079a4ff0b1e6f51bea6c2bd1bb428
Andrea Coronese <sixpounder@protonmail.com>
2021-04-06 16:47:23 +0200

    prophanities were produced here

--- src/linalg.rs
+++ src/linalg.rs
@@ -28,24 +28,30 @@ pub fn matmul<T: Num + Copy>(a: Tensor<T>, b: Tensor<T>) -> Result<Tensor<T>, Te
 
         let mut out_tensor = Tensor::<T>::new_uninit(target_shape.clone());
 
-        let op_len = target_shape[1];
+        let series_length = target_shape[1];
         let mut row_number = 0;
         while row_number < target_shape[0] {
+            // The components of the row
             let this_row: Vec<TensorComponent<T>> =
-                a.iter().skip(row_number).take(op_len).collect();
+                a.iter().skip(row_number * series_length).take(series_length).collect();
+
+            // Multiply for each column components
             let mut col_number = 0;
-            while col_number < op_len {
+            while col_number < series_length {
+                // N-th column components
                 let this_col: Vec<TensorComponent<T>> = b
                     .iter()
-                    .skip(col_number)
-                    .step_by(op_len)
-                    .take(op_len)
+                    .skip(col_number) // Start from the col_number-th element, to jump columns
+                    .step_by(series_length) // One element each op_len, to jump rows
+                    .take(series_length) // Take op_len elements to much row elements
                     .collect();
-                // assert_eq!(this_row.len(), this_col.len());
+
+                // Run the series
                 let mut acc = T::zero();
-                for i in 0..op_len {
+                for i in 0..series_length {
                     acc = acc + (*(this_row[i]).value * *(this_col[i]).value);
                 }
+
                 out_tensor[coord!(row_number, col_number)] = acc;
                 col_number += 1;
             }
@@ -93,49 +99,65 @@ where
             return Err(TensorError::NotSupported);
         }
 
-        let op_len = target_shape[1];
+        let series_length = target_shape[1];
         let mut out_tensor = Tensor::<T>::new_uninit(target_shape.clone());
         let mut handles = vec![];
         let matrices = Arc::new(Mutex::new((a, b)));
 
-        for _ in 0..target_shape.mul() {
+        for nth_iteration in 0..target_shape[0] {
+            // nth_iteration spans from 0 to the number of rows - 1 (i.e. row index)
+
+            // Build thread local variables
             let local_matrices = Arc::clone(&matrices);
-            let coords = coord!(0, 0);
-            let t_op_len = op_len;
+            // let coords = coord!(nth_iteration, 0);
+            let row_index = nth_iteration;
+            let t_series_length = series_length;
             let calculate = move || {
+                // Retrieve A and B matrices
                 let ab = local_matrices
                     .lock()
                     .expect("Failed to acquire lock on resource");
                 let local_a = &ab.0;
                 let local_b = &ab.1;
-                let row = coords[0];
-                let col = coords[1];
-                let mut acc = T::zero();
-                let this_row: Vec<TensorComponent<T>> =
-                    local_a.iter().skip(row * t_op_len).take(t_op_len).collect();
-                let this_col: Vec<TensorComponent<T>> = local_b
-                    .iter()
-                    .skip(col * t_op_len)
-                    .step_by(t_op_len)
-                    .take(t_op_len)
-                    .collect();
-                for i in 0..t_op_len {
-                    acc = acc + (*(this_row[i]).value * *(this_col[i]).value);
-                }
-                MatmulComponent {
-                    row,
-                    col,
-                    value: acc,
-                }
-            };
-            let t = thread::spawn(calculate);
 
+                let mut results = vec![];
+                let mut acc = T::zero();
+
+                let this_row: Vec<TensorComponent<T>> =
+                    local_a.iter().skip(row_index * t_series_length).take(t_series_length).collect();
+
+                for col_number in 0..series_length {
+                    let this_col: Vec<TensorComponent<T>> = local_b
+                        .iter()
+                        .skip(col_number)
+                        .step_by(t_series_length)
+                        .take(t_series_length)
+                        .collect();
+                        for i in 0..t_series_length {
+                            acc = acc + (*(this_row[i]).value * *(this_col[i]).value);
+                        }
+
+                        results.push(
+                            MatmulComponent {
+                                row: row_index,
+                                col: col_number,
+                                value: acc,
+                            }
+                        );
+                }
+
+                results
+            };
+
+            let t = thread::spawn(calculate);
             handles.push(t);
         }
 
         for h in handles {
-            let result = h.join().unwrap();
-            out_tensor[coord!(result.row, result.col)] = result.value;
+            let results = h.join().unwrap();
+            for result in results.iter() {
+                out_tensor[coord!(result.row, result.col)] = result.value;
+            }
         }
         Ok(out_tensor)
     }
@@ -165,12 +187,17 @@ mod test {
         t2[coord!(2, 1)] = 4;
         t2[coord!(2, 2)] = 6;
 
+        let start = std::time::Instant::now();
         let mul_res: Result<Tensor<i32>, TensorError> = super::matmul(t1, t2);
+        let end = std::time::Instant::now();
+        let duration = std::time::Duration::from(end - start);
+        println!("{}", duration.as_nanos());
         assert!(mul_res.is_ok());
         let t3 = mul_res.unwrap();
         assert_eq!(t3.shape(), &shape!(2, 3));
         assert_eq!(t3[coord!(0, 0)], 13); // [1, 2, 4] dot [1, 2, 2]
         assert_eq!(t3[coord!(0, 1)], 26); // [1, 2, 4] dot [2, 4, 4]
+        assert_eq!(t3[coord!(1, 1)], 44); // [2, 4, 6] dot [2, 4, 4]
     }
 
     #[test]
@@ -194,12 +221,38 @@ mod test {
         t2[coord!(2, 1)] = 4;
         t2[coord!(2, 2)] = 6;
 
+        let start = std::time::Instant::now();
         let mul_res: Result<Tensor<i32>, TensorError> = super::matmul_threaded(t1, t2);
+        let end = std::time::Instant::now();
+        let duration = std::time::Duration::from(end - start);
+        println!("{}", duration.as_nanos());
+
         assert!(mul_res.is_ok());
         let t3 = mul_res.unwrap();
-        println!("{:?}", t3);
         assert_eq!(t3.shape(), &shape!(2, 3));
         assert_eq!(t3[coord!(0, 0)], 13); // [1, 2, 4] dot [1, 2, 2]
         assert_eq!(t3[coord!(0, 1)], 26); // [1, 2, 4] dot [2, 4, 4]
+        assert_eq!(t3[coord!(1, 1)], 44); // [2, 4, 6] dot [2, 4, 4]
+    }
+
+    #[test]
+    fn bench_matmul_very_big_inputs() {
+        let t1: Tensor<i32> = Tensor::new(shape!(40, 40), Some(&|_, _| { 1 }));
+        let t2: Tensor<i32> = Tensor::new(shape!(40, 40), Some(&|_, _| { 1 }));
+        let start = std::time::Instant::now();
+        let _mul_res: Result<Tensor<i32>, TensorError> = super::matmul(t1, t2);
+        let end = std::time::Instant::now();
+        let duration = std::time::Duration::from(end - start);
+        assert!(duration.as_millis() < 3000);
+    }
+    #[test]
+    fn bench_matmul_threaded_very_big_inputs() {
+        let t1: Tensor<i32> = Tensor::new(shape!(40, 40), Some(&|_, _| { 1 }));
+        let t2: Tensor<i32> = Tensor::new(shape!(40, 40), Some(&|_, _| { 1 }));
+        let start = std::time::Instant::now();
+        let _mul_res: Result<Tensor<i32>, TensorError> = super::matmul_threaded(t1, t2);
+        let end = std::time::Instant::now();
+        let duration = std::time::Duration::from(end - start);
+        assert!(duration.as_millis() < 3000);
     }
 }
